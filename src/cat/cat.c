35c717b22a378f063d77a4fd132989cc3729aca7
Comedian Likt <comedial@ge-e3.kzn.21-school.ru>
2022-10-29 12:56:02 +0300

    works with options b, e, n, t

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -1,85 +1,128 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
+#include<getopt.h>
 
+typedef struct {
+    int b_flag;
+    int e_flag;
+    int n_flag;
+    int s_flag;
+    int t_flag;
+    int v_flag;
+    int E_flag;
+    int T_flag;
+} catOptions;
 
-int readFile(char **argv, int currentFile, FILE **f);
-void output(char *buffer, FILE *f, int *num, int b_flag);
-void bOpt(char **buffer, int *num);
-int options(int argc, char **argv, int* b_flag);
+void output(char c, int *position, int *num, catOptions opt);
+int options(int argc, char **argv, catOptions *opt);
 
 int main(int argc, char **argv) {
+    char c;
     FILE *f;
-    char buffer[10000];
-    int currentFile = 1;
-    int num = 1; // for b opt
-    int b_flag = 0; // for b opt
-    
+    int position = 0, num = 1, currentFile = 1;
+    catOptions opt = {0};
     if (argc > 1) {
-        if (options(argc, argv, &b_flag))
-            currentFile += 1;
-        while (currentFile < argc) {
-            if(readFile(argv, currentFile, &f)) {
-                output(buffer, f, &num, b_flag);
-            currentFile++;
+        options(argc, argv, &opt);
+        while(currentFile < argc) {
+            f = fopen(argv[currentFile], "r");
+            if (f == NULL) {
+              printf("%s: %s: No such file or directory\n", argv[0], argv[currentFile]);
+            } else {
+                while((c = fgetc(f)) != EOF) {
+                    output(c, &position, &num, opt);
+                    
+                }
+            }
+            fclose(f);
             num = 1;
+            position = 0;
+            currentFile++;
         }
     }
-    }
+    
     return 0;
 }
 
-int readFile(char **argv, int currentFile, FILE **f) {
-  *f = fopen(argv[currentFile], "r");
-    if (*f == NULL) {
-      printf("%s: %s: No such file or directory\n", argv[0], argv[currentFile]);
-      return 0;
+void output(char c, int *position, int *num, catOptions opt) {
+    if(opt.b_flag) {
+      if(c != '\n' && *position == 0) {
+            printf("%6d\t", *num);
+            *position+=1;
+            *num+=1;
+      }
+          printf("%c", c);
+        if(c == '\n')
+            *position = 0;
+    }
+    if(opt.e_flag) {
+      if(c == '\n') {
+        printf("$");
+      }
+    printf("%c", c);
+    }
+    if(opt.n_flag) {
+      if(*position == 0) {
+            printf("%6d\t", *num);
+            *position+=1;
+            *num+=1;
+        }
+            printf("%c", c);
+
+        if(c == '\n')
+            *position = 0;
+    }
+//    if(opt.s_flag) {
+//      if(c == '\n' && *position == 0) {
+//          while(c == '\n') {
+//              break;
+//          }
+//      *position+=1;
+//      printf("\n");
+//      } else {
+//         printf("%c", c);
+//      }
+//      if(c == '\n')
+//        *position = 0;
+//    }
+    if(opt.t_flag) {
+      if(c == '\t') {
+        printf("^I");
+      } else {
+          printf("%c", c);
+      }
+    }
+    
+}
+
+int options(int argc, char **argv, catOptions *opt) {
+    char ch;
+    static struct option long_option[] = {
+        {"number-nonblank", 0, NULL, 'b'},
+        {"number", 0, NULL, 'n'},
+        {"squeeze-blank", 0, NULL, 's'},
+        {NULL, 0, NULL, 0}
+    };
+    while((ch = getopt_long(argc, argv, "+benstET", long_option, NULL)) != -1) {
+      switch(ch) {
+        case 'b':
+              opt->b_flag = 1; break;
+        case 'e':
+              opt->e_flag = 1;
+              opt->v_flag = 1; break;
+        case 'n':
+              opt->n_flag = 1; break;
+        case 's':
+              opt->s_flag = 1; break;
+        case 't':
+              opt->t_flag = 1;
+              opt->v_flag = 1; break;
+        case 'E':
+              opt->E_flag = 1; break;
+        case 'T':
+              opt->T_flag = 1; break;
+          default:
+              return 0;
+      }
     }
     return 1;
 }
 
-void output(char *buffer, FILE *f, int *num, int b_flag) {
-    while (feof(f) == 0) {
-        fgets(buffer, 10000, f);
-        if (b_flag) {
-            bOpt(&buffer, num);
-        }
-        printf("%s", buffer);
-        }
-}
-
-int options(int argc, char **argv, int* b_flag) {
-    char opt;
-    while((opt = getopt(argc, argv, "benstv")) != -1) {
-      switch(opt) {
-        case 'b':
-              *b_flag+=1; break;
-        case 'e':
-            break;
-        case 'n':
-            break;
-        case 's':
-            break;
-        case 't':
-            break;
-        case 'v':
-            break;
-          default:
-              printf("no flags"); //don't forget to delete
-              return 0;
-      }
-    }
-    return *b_flag;
-}
-
-void bOpt(char **buffer, int *num) {
-    int len = strlen(*buffer);
-    char str[10000];
-    sprintf(str, "%6d\t", *num);
-    if(len != 1) {
-        strcat(str, *buffer);
-        strcpy(*buffer, str);
-        *num+=1;
-    }
-}
