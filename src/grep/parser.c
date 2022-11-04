3b25b75805daba4f5d5636210170385d935891db
Comedian Likt <comedial@et-c5.kzn.21-school.ru>
2022-11-04 17:12:00 +0300

    работающий парсер

--- src/grep/parser.c
+++ src/grep/parser.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include<string.h>
-#include<stdlib.h>
 
 typedef struct {
   int e_flag;
@@ -15,36 +15,41 @@ typedef struct {
   int o_flag;
 } grepOptions;
 
-
-
-int getOption(int argc, char** argv, grepOptions *opt, char** patterns);
+int getOption(int argc, char** argv, grepOptions *opt, char** patterns, int *numOfpatterns);
 int parser(grepOptions *opt, int i, char** argv, int *numOfpatterns, char** patterns);
+void patternForE(char** argv, int i, int k, int* l, char** patterns);
 void copyEOstr (char *dest, char *src, int k);
-void patternForE(char** argv, int i, int k, int* n, char** patterns);
-char* patternWithoutE (int argc, char** argv);
 
-int main(int argc, char** argv) {
-    char* patterns[100];
-    for (int i = 0; i < 100; i++)
+int main(int argc, char **argv) {
+    char* patterns[10];
+    for (int i = 0; i < 10; i++)
         patterns[i] = NULL;
     grepOptions opt = {0};
-    getOption(argc, argv, &opt, patterns);
+    int numOfpatterns = 0;
+    
+    getOption(argc, argv, &opt, patterns, &numOfpatterns);
+    
 //    for (int i = 0; i < argc; i++)
-//      if(argv[i] == NULL)
-//        printf("i = %d: %s \n", i, argv[i]);
-    printf("e_flag = %d\nc_flag = %d\no_flag = %d\nn_flag = %d\n", opt.e_flag, opt.c_flag, opt.o_flag, opt.n_flag);
+//      printf("i = %d: %s \n", i, argv[i]);
+    printf("e_flag = %d\nc_flag = %d\no_flag = %d\nn_flag = %d\nl_flag = %d\ni_flag = %d\n", opt.e_flag, opt.c_flag, opt.o_flag, opt.n_flag, opt.l_flag, opt.i_flag);
     printf("patterns[0] = %s\n", patterns[0]);
     printf("patterns[1] = %s\n", patterns[1]);
     printf("patterns[2] = %s\n", patterns[2]);
-    for (int i = 0; i < 100; i++)
+    printf("numOfpatterns = %d\n", numOfpatterns);
+    for (int i = 0; i < numOfpatterns; i++)  {
+        printf("patterns[%d] was freed= %s\n", i, patterns[i]);
         free(patterns[i]);
+            }
+  return 0;
 }
 
-int getOption(int argc, char** argv, grepOptions *opt, char** patterns) {
-    int i = 1, numOfpatterns = 0;
+
+
+int getOption(int argc, char** argv, grepOptions *opt, char** patterns, int *numOfpatterns) {
+    int i = 1;
     while(i < argc) {
         if(argv[i][0] == '-') {
-            parser(opt, i, argv, &numOfpatterns, patterns);
+            parser(opt, i, argv, numOfpatterns, patterns);
             memset(argv[i], '\0', strlen(argv[i]));
         }
         i++;
@@ -96,18 +101,7 @@ int parser(grepOptions *opt, int i, char** argv, int *numOfpatterns, char** patt
 }
 
 
-// копирует src в dest начиная с k-го элемента
-void copyEOstr (char *dest, char *src, int k) {
-int m = 0;
-while(src[k] != '\0') {
-    dest[m] = src[k];
-    m++;
-    k++;
-}
-    memset(src, '\0', strlen(src));
-}
 
-    
 // парсим шаблон после флага -е
 void patternForE(char** argv, int i, int k, int* l, char** patterns) {
     if (argv[i][k + 1] != '\0') {
@@ -123,9 +117,14 @@ void patternForE(char** argv, int i, int k, int* l, char** patterns) {
     *l+=1; // l = numOfpatterns
 }
 
-char* patternWithoutE (int argc, char** argv) {
-    while(i < argc) {
-        if (argv[i][0] != '\0')
-            return argv[i];
-    }
+
+// копирует src в dest начиная с k-го элемента
+void copyEOstr (char *dest, char *src, int k) {
+int m = 0;
+while(src[k] != '\0') {
+    dest[m] = src[k];
+    m++;
+    k++;
+}
+//    memset(src, '\0', strlen(src)); // лишнее?
 }
